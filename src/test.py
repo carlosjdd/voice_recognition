f63d9ff408964124223fbababbc7c7abfa24705d
carlosjdd <carlosjdd@users.noreply.github.com>
2020-10-22 15:10:12 +0200

    First trial for recognition

---
+++ src/test.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+audio = "Prueba.wav"
+
+re = sr.Recognizer()
+
+with sr.AudioFile(audio) as source:
+        info_audio=re.record(source)
+        texto=re.recognize_google(info_audio)
+        print(texto)
