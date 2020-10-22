6d83eeffcdfd4d3d97d8efc4bf3312e0996dc79b
carlosjdd <carlosjdd@users.noreply.github.com>
2020-10-22 15:27:53 +0200

    New test audio

--- src/test.py
+++ src/test.py
@@ -7,4 +7,6 @@ re = sr.Recognizer()
 with sr.AudioFile(audio) as source:
         info_audio=re.record(source)
         texto=re.recognize_google(info_audio)
+        print("El texto reconocido es:\n")
         print(texto)
+        print("\nSaliendo\n")
