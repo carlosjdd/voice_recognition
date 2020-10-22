c2496d77ac87137dde5f6e57b2e53972d14d034b
carlosjdd <carlosjdd@users.noreply.github.com>
2020-10-22 15:33:25 +0200

    Test in Spanish and English

--- src/test.py
+++ src/test.py
@@ -1,12 +1,20 @@
 import speech_recognition as sr
 
 audio = "Prueba.wav"
+audio2 = "Test_en.wav"
 
 re = sr.Recognizer()
 
 with sr.AudioFile(audio) as source:
-        info_audio=re.record(source)
-        texto=re.recognize_google(info_audio)
-        print("El texto reconocido es:\n")
-        print(texto)
-        print("\nSaliendo\n")
+    info_audio=re.record(source)
+    texto=re.recognize_google(info_audio, language="es-ES")
+    print("El texto reconocido es:\n")
+    print(texto)
+    print("\nAhora en Ingles:\n")
+
+with sr.AudioFile(audio2) as source:
+    info_audio=re.record(source)
+    texto=re.recognize_google(info_audio)
+    print("El texto reconocido es:\n")
+    print(texto)
+    print("\nAhora en Ingles:\n")
