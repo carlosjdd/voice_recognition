0252ddeec9fbded979154f8940f575f3df351135
carlosjdd <carlosjdd@users.noreply.github.com>
2020-11-05 14:47:39 +0100

    Add functionalities to qbo

--- src/long_recognizer.py
+++ src/long_recognizer.py
@@ -9,6 +9,7 @@ import csv
 # import the necessary msgs. Example with msg type String_Int_Arrays:
 from std_msgs.msg import String
 from std_msgs.msg import UInt16MultiArray
+from std_msgs.msg import UInt8
 
 
 class detector():
@@ -29,13 +30,19 @@ class detector():
 
         #Subscribe to ROS topics
         self.detector_sub = rospy.Subscriber("asr_full_text", String, self.callback)
+        self.mode_sub = rospy.Subscriber("mode", UInt8, self.mode_cb)
 
         #Define the ROS publishers
         self.fragance_pub = rospy.Publisher("fragances", UInt16MultiArray, queue_size=0)
+        self.function_pub = rospy.Publisher("function", UInt8, queue_size=0)
 
         #Define object as msg type
         self.fragance_msg = UInt16MultiArray()
         self.fragance_msg.data = []
+
+        self.function_msg = UInt8()
+
+        self.mode = 2
 
         self.databases()
 
@@ -44,11 +51,14 @@ class detector():
     def databases(self):
         rospack = rospkg.RosPack()
         pkg_name = "voice_recognition"			# Name of the ROS package. Is used to take the path of the package
-        path_fragances = rospack.get_path(pkg_name) + "/data/fragances.csv"
+        path_fragances = rospack.get_path(pkg_name) + "/data/long_fragances.csv"
+        path_options = rospack.get_path(pkg_name) + "/data/long_options.csv"
 
         self.word = []
         self.frag1 = []
         self.frag2 = []
+        self.options = []
+        self.modes = []
 
         with open(path_fragances) as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=";")	            # Read the csv file
@@ -57,25 +67,43 @@ class detector():
                 self.frag1.append(row[1])
                 self.frag2.append(row[2])
 
+        with open(path_fragances) as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=";")	            # Read the csv file
+            for row in csv_reader:								        # Go through every row in the csv file
+                self.options.append(row[0])					            # Save the path of every SVG file into the array
+                self.modes.append(row[1])
+
     def detect_word(self, phrase):
 
-        self.fragance_msg.data = []
-        check = False
+        if self.mode == 0:
+            detected = False
+            for j in range(len(self.options)):
+                if phrase.find(self.options[j]) >= 0:
+                    detected = True
+                    self.function_msg.data = int(self.modes[j])
+            if detected == False:
+                self.function_msg.data = 2                          # If modes not found, lets ask Alexa
+            self.function_pub.publish(self.function_msg)
 
-        for j in range(len(self.word)):
-            if phrase.find(self.word[j]) >= 0:
-                if int(self.frag1[j]) in self.fragance_msg.data:
-                    pass
-                else:
-                    self.fragance_msg.data.append(int(self.frag1[j]))
-                if int(self.frag2[j]) != 0:
-                    if int(self.frag2[j]) in self.fragance_msg.data:
+        elif self.mode == 1:
+            self.fragance_msg.data = []
+            for j in range(len(self.word)):
+                if phrase.find(self.word[j]) >= 0:
+                    if int(self.frag1[j]) in self.fragance_msg.data:
                         pass
                     else:
-                        self.fragance_msg.data.append(int(self.frag2[j]))
-                print (self.word[j])
+                        self.fragance_msg.data.append(int(self.frag1[j]))
+                    if int(self.frag2[j]) != 0:
+                        if int(self.frag2[j]) in self.fragance_msg.data:
+                            pass
+                        else:
+                            self.fragance_msg.data.append(int(self.frag2[j]))
+                    print (self.word[j])
+            self.fragance_pub.publish(self.fragance_msg)
+            
+        elif self.mode == 2:
+            continue
 
-        self.fragance_pub.publish(self.fragance_msg)
 
     def run_loop(self):
         """ Infinite loop.
@@ -97,6 +125,12 @@ class detector():
 
         This void is executed when a message is received"""
         self.detect_word(data.data)
+
+    def mode_cb(self, data):
+        """ROS callback
+
+        This void is executed when a message is received"""
+        self.mode = data.data
 
 
 if __name__=='__main__':
